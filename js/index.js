93e5484224f93409b639b200d90ec3946b6b41e5
Henrique Palazzo <henriquepalazzo@gmail.com>
2024-02-19 10:23:06 +0100

    Add functionality to the bookmark button and transfer the SVG to the CSS file as background.

--- js/index.js
+++ js/index.js
@@ -1,9 +1,16 @@
 const button = document.querySelectorAll("button")
+const bookmark = document.querySelectorAll(".bookmark")
 
 button.forEach((element)=>{
     element.addEventListener("click", (event) => {
         const cardAnswer = event.target.parentElement.querySelector(".answer")
         cardAnswer.classList.toggle("hidden")
         event.target.textContent = cardAnswer.classList.contains("hidden") ? "Show answer" : "Close answer"
+    })
+})
+
+bookmark.forEach((element)=>{
+    element.addEventListener("click", (event) => {
+        event.target.classList.toggle("inactive")
     })
 })
